261ab3379ec36e99d5d9fe14b1546e117276ddf6
Bryan Fernandes <bryanfernandes.dev@gmail.com>
2026-08-15 18:17:00 -0300

    Initial commit

---
+++ MenuCalculadora/src/ConversorNumeros.java
@@ -0,0 +1,14 @@
+public class ConversorNumeros {
+
+    public Integer StringToInt(String num)
+    {
+        int conversor = Integer.parseInt(num);
+        return (conversor);
+    }
+
+    public Double stringToDouble(String num)
+    {
+        double conversor = Double.parseDouble(num);
+        return (conversor);
+    }
+}
---
+++ MenuCalculadora/src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args)
+    {
+        MenuCalculadora menu = new MenuCalculadora();
+        menu.executarCalculadora();
+    }
+}
